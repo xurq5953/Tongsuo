83d37a08d33d79b8a5edda4318090203e99ffc05
Xiaoman <weikaizhao@zju.edu.cn>
2026-01-09 16:58:07 +0800

    separate SM2 and NIST optimization compilation

--- crypto/ec/ecp_sm2p256.c
+++ crypto/ec/ecp_sm2p256.c
@@ -6,6 +6,7 @@
  * in the file LICENSE in the source distribution or at
  * https://www.openssl.org/source/license.html
  *
+ * The optimization of SM2 was also inspired by GuanZhi‘s work on GMSSL.
  */
 
 /*
